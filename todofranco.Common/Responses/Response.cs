c9914238e6562b77e5194074b4c32325cdc5bd83
FERNANDO FRANCO MARTINEZ <ferfranco.uy@hotmail.com>
2021-08-30 22:38:43 -0400

    Add Entity

---
+++ todofranco.Common/Responses/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace todofranco.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSuccesss { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
